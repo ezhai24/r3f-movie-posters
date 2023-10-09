4d89204a455e43937c8931ad23f3aede317d6c02
Emily Zhai <ezhai24@gmail.com>
2023-10-08 20:20:55 -0400

    make plane draggable

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
-import { Canvas } from "@react-three/fiber";
+import { animated, useSpring } from "@react-spring/three";
+import { Canvas, useThree } from "@react-three/fiber";
+import { useDrag } from "@use-gesture/react";
+
+const Scene = () => {
+  const { viewport } = useThree();
+  const [spring, api] = useSpring(() => ({ position: [0, 0, 0] }));
+  useDrag(
+    ({ offset: [offsetX] }) => {
+      api.start({ position: [offsetX, 0, 0] });
+    },
+    {
+      target: window.document,
+      transform: ([x, y]) => [x / viewport.factor, y / viewport.factor],
+    }
+  );
+
+  return (
+    <>
+      <ambientLight />
+      <animated.mesh {...spring}>
+        <planeGeometry />
+        <meshStandardMaterial />
+      </animated.mesh>
+    </>
+  );
+};
 
 const Home = () => {
   return (
     <Canvas>
-      <ambientLight />
-      <mesh>
-        <planeGeometry />
-        <meshStandardMaterial />
-      </mesh>
+      <Scene />
     </Canvas>
   );
 };
