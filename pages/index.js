6de5c4d07ef4b5f205ae65f8f34a7b677b32515e
Emily Zhai <ezhai24@gmail.com>
2023-10-08 20:58:39 -0400

    move multiple posters on drag

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,17 @@ import { animated, useSpring } from "@react-spring/three";
 import { Canvas, useThree } from "@react-three/fiber";
 import { useDrag } from "@use-gesture/react";
 
-const Scene = () => {
+const Poster = ({ initialPosition, color }) => {
   const { viewport } = useThree();
-  const [spring, api] = useSpring(() => ({ position: [0, 0, 0] }));
+  const [spring, api] = useSpring(() => ({ position: initialPosition }));
   useDrag(
-    ({ offset: [offsetX] }) => {
-      api.start({ position: [offsetX, 0, 0] });
+    ({ movement: [movementX], first, memo = { dragStart: 0 } }) => {
+      if (first) {
+        const [currentX] = spring.position.get();
+        return { dragStart: currentX };
+      }
+      const updatedX = memo.dragStart + movementX;
+      api.start({ position: [updatedX, 0, 0] });
     },
     {
       target: window.document,
@@ -16,12 +21,20 @@ const Scene = () => {
   );
 
   return (
+    <animated.mesh {...spring}>
+      <planeGeometry />
+      <meshStandardMaterial color={color} />
+    </animated.mesh>
+  );
+};
+
+const Scene = () => {
+  return (
     <>
       <ambientLight />
-      <animated.mesh {...spring}>
-        <planeGeometry />
-        <meshStandardMaterial />
-      </animated.mesh>
+
+      <Poster initialPosition={[-2, 0, 0]} color="orange" />
+      <Poster initialPosition={[0, 0, 0]} color="blue" />
     </>
   );
 };
