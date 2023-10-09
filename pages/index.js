b0c28c0e821c5968933464776328c2b7fccb504e
Emily Zhai <ezhai24@gmail.com>
2023-10-08 21:24:02 -0400

    rotate posters when scroll bounds are crossed

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,34 @@ const POSTER_SIZE = { width: 1, height: 1 };
 const POSTER_SPACING = 1;
 const POSTER_DISTANCE = POSTER_SIZE.width + POSTER_SPACING;
 
-const Poster = ({ initialPosition, color }) => {
+const Poster = ({
+  initialPosition,
+  color,
+  scrollBounds: [leftScrollBound, rightScrollBound],
+}) => {
   const { viewport } = useThree();
   const [spring, api] = useSpring(() => ({ position: initialPosition }));
   useDrag(
-    ({ movement: [movementX], first, memo = { dragStart: 0 } }) => {
+    ({
+      movement: [movementX],
+      first,
+      memo = { dragStart: 0, movementOffset: 0 },
+    }) => {
       if (first) {
         const [currentX] = spring.position.get();
-        return { dragStart: currentX };
+        return { dragStart: currentX, movementOffset: 0 };
       }
-      const updatedX = memo.dragStart + movementX;
+      const updatedX = memo.dragStart + movementX + memo.movementOffset;
       api.start({ position: [updatedX, 0, 0] });
+
+      if (updatedX < leftScrollBound) {
+        api.set({ position: [rightScrollBound, 0, 0] });
+        return { dragStart: rightScrollBound, movementOffset: -1 * movementX };
+      }
+      if (updatedX > rightScrollBound) {
+        api.set({ position: [leftScrollBound, 0, 0] });
+        return { dragStart: leftScrollBound, movementOffset: -1 * movementX };
+      }
     },
     {
       target: window.document,
@@ -36,6 +53,9 @@ const Scene = () => {
   const posters = ["orange", "blue"];
   const postersStart = -Math.floor(posters.length / 2) * POSTER_DISTANCE;
 
+  const scrollBoundsFactor = Math.ceil(posters.length / 2) * POSTER_DISTANCE;
+  const scrollBounds = [-scrollBoundsFactor, scrollBoundsFactor];
+
   return (
     <>
       <ambientLight />
@@ -44,6 +64,7 @@ const Scene = () => {
           key={poster}
           initialPosition={[postersStart + i * POSTER_DISTANCE, 0, 0]}
           color={poster}
+          scrollBounds={scrollBounds}
         />
       ))}
     </>
