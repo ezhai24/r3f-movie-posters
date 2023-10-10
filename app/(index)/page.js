9419036a6bf4f077c97da6f447967f49e414e74e
Emily Zhai <ezhai24@gmail.com>
2023-10-09 22:37:20 -0400

    fix poster wiggle bug

--- app/(index)/page.js
+++ app/(index)/page.js
@@ -13,43 +13,72 @@ const POSTER_SPACING = 1;
 const POSTER_DISTANCE = POSTER_SIZE.width + POSTER_SPACING;
 
 const Poster = ({
-  initialPosition,
   color,
-  posterBounds: [leftPosterBound, rightPosterBound],
+  initialX,
+  postersGroupWidth,
   scrollBounds: [leftScrollBound, rightScrollBound],
 }) => {
   const { viewport } = useThree();
-  const [spring, api] = useSpring(() => ({ position: initialPosition }));
+  const [spring, api] = useSpring(() => ({
+    position: [initialX, 0, 0],
+  }));
   useDrag(
-    ({
-      movement: [movementX],
-      first,
-      memo = { dragStart: 0, movementOffset: 0 },
-      active,
-    }) => {
-      if (first) {
-        const [currentX] = spring.position.get();
-        return { dragStart: currentX, movementOffset: 0 };
-      }
-      const updatedX = memo.dragStart + movementX + memo.movementOffset;
-      api.start({ position: [updatedX, 0, 0] });
+    ({ offset: [offsetX] }) => {
+      const posterX = offsetX + spring.position.get()[0];
 
-      // Rotate posters when scroll bounds are crossed
-      if (updatedX < leftScrollBound) {
-        api.set({ position: [rightPosterBound, 0, 0] });
-        return { dragStart: rightPosterBound, movementOffset: -1 * movementX };
-      }
-      if (updatedX > rightScrollBound) {
-        api.set({ position: [leftPosterBound, 0, 0] });
-        return { dragStart: leftPosterBound, movementOffset: -1 * movementX };
+      // If the poster is within the scroll bounds, no adjustments
+      // need to be made. We maintain the offset it has from the
+      // poster group center's position.
+      if (leftScrollBound <= posterX && posterX <= rightScrollBound) {
+        return;
       }
 
-      // On drag end, snap posters to nearest snapping point
-      if (!active) {
-        const snappingPoint =
-          POSTER_DISTANCE * Math.round(updatedX / POSTER_DISTANCE);
-        api.start({ position: [snappingPoint, 0, 0] });
-      }
+      // Calculate the number of times the poster has crossed a
+      // scroll bound.
+      //
+      // This number of crosses can also be thought of as the
+      // number of times the poster has traversed the poster
+      // group's width (pGW) after crossing either the
+      // rightScrollBound (rSB) or the leftScrollBound (lSB):
+      //     |<---2--->|<---1--->|<--0-->|<---1--->|<---2--->|
+      // lSB-2*pGW  lSB-pgW     lSB  0  rSB     lSB+pGW  lSB+2*pgW
+      //
+      // Numerically we can calculate this as:
+      // - The position of the poster with no adjustments = the
+      //   position of the poster group center + the distance
+      //   between the poster group center and the poster
+      //   = offsetX + initialX
+      // - Minus the distance between the screen center and the bound
+      // - Divided by the poster group's width (pGW)
+      // - Rounded to the next upper number (ie. -1.2 -> -2, 1.2 -> 2).
+      //   This means ceiling for positive numbers and flooring for
+      //   negative numbers.
+      const numCrosses =
+        posterX > rightScrollBound
+          ? Math.ceil(
+              (offsetX + initialX - rightScrollBound) / postersGroupWidth
+            )
+          : Math.floor(
+              (offsetX + initialX - leftScrollBound) / postersGroupWidth
+            );
+
+      // Adjust the position of the poster in relation to the poster
+      // group center's position.
+      //
+      // We can calculate the adjustment as:
+      // - The poster group's width * the number of times the poster
+      //   has crossed the left/right scroll bound.
+      // - A negative numCrosses indicates we have crossed the left
+      //   scroll bound numCrosses times. In this case, we want to
+      //   adjust the poster to the far right. Similarly, a positive
+      //   numCrosses indicates crosses of the right bound and
+      //   necessitates an adjustment to the far left. Multiply by
+      //   -1 to account for this inversion.
+      // - Offset by the distance between the poster group center
+      //   and the poster (initialX)
+      api.set({
+        position: [postersGroupWidth * numCrosses * -1 + initialX, 0, 0],
+      });
     },
     {
       target: window.document,
@@ -66,26 +95,40 @@ const Poster = ({
 };
 
 const Scene = () => {
-  const posters = ["orange", "blue", "green", "pink"];
-  const postersPositionFactor =
-    Math.floor(posters.length / 2) * POSTER_DISTANCE;
-  const posterBounds = [-postersPositionFactor, postersPositionFactor];
+  const posters = ["orange", "blue", "pink", "green"];
 
   const scrollBoundsFactor = Math.ceil(posters.length / 2) * POSTER_DISTANCE;
   const scrollBounds = [-scrollBoundsFactor, scrollBoundsFactor];
 
+  const { viewport } = useThree();
+  const [spring, api] = useSpring(() => ({ position: [0, 0, 0] }));
+  useDrag(
+    ({ offset: [offsetX] }) => {
+      api.set({ position: [offsetX, 0, 0] });
+    },
+    {
+      target: window.document,
+      transform: ([x, y]) => [x / viewport.factor, y / viewport.factor],
+    }
+  );
+
   return (
     <>
       <ambientLight />
-      {posters.map((poster, i) => (
-        <Poster
-          key={poster}
-          initialPosition={[posterBounds[0] + i * POSTER_DISTANCE, 0, 0]}
-          color={poster}
-          posterBounds={posterBounds}
-          scrollBounds={scrollBounds}
-        />
-      ))}
+      <animated.group {...spring}>
+        {posters.map((poster, i) => (
+          <Poster
+            key={poster}
+            color={poster}
+            initialX={
+              -Math.floor(posters.length / 2) * POSTER_DISTANCE +
+              i * POSTER_DISTANCE
+            }
+            postersGroupWidth={posters.length * POSTER_DISTANCE}
+            scrollBounds={scrollBounds}
+          />
+        ))}
+      </animated.group>
 
       {DEBUG && (
         <Debug
