89e3044ba5df3bb6c320e05c6abea6236ec39b57
Emily Zhai <ezhai24@gmail.com>
2023-10-10 17:50:17 -0400

    update metadata pattern to match current version of next

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,14 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Emily Zhai | Movie Slider",
+  description:
+    "An infinite slider of movie posters. A react-three-fiber learning experiment by Emily Zhai.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
       <body>{children}</body>
     </html>
   );
